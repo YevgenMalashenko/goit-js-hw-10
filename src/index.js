10c8af39dbc6dde4003550b1a8b0e779241df37a
YevgenMalashenko <malashenkoyevgen1986@gmail.com>
2022-08-22 20:50:42 +0300

    step1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+
+const inputEl = document.querySelector('#search-box');
+console.log(inputEl);
+
+const countryListEl = document.querySelector('.country-list');
+
+inputEl.addEventListener('input', debounce(onFormInput, DEBOUNCE_DELAY));
+
+// Notify.failure('Oops, there is no country with that name');
+// Notify.info('Too many matches found. Please enter a more specific name.');
+
+function onFormInput(event) {
+  event.preventDefault();
+  countryListEl.innerHTML = `<li>${event.target.value}</li>`;
+}
