4f919ef2ab2af729a261b085000b319a7996125d
YevgenMalashenko <malashenkoyevgen1986@gmail.com>
2022-08-24 12:34:01 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,16 +5,34 @@ import debounce from 'lodash.debounce';
 const DEBOUNCE_DELAY = 300;
 
 const inputEl = document.querySelector('#search-box');
-console.log(inputEl);
+// console.log(inputEl);
 
 const countryListEl = document.querySelector('.country-list');
 
 inputEl.addEventListener('input', debounce(onFormInput, DEBOUNCE_DELAY));
 
-// Notify.failure('Oops, there is no country with that name');
-// Notify.info('Too many matches found. Please enter a more specific name.');
+Notify.failure('Oops, there is no country with that name');
+Notify.info('Too many matches found. Please enter a more specific name.');
 
 function onFormInput(event) {
   event.preventDefault();
-  countryListEl.innerHTML = `<li>${event.target.value}</li>`;
+  const name = event.target.value;
+  fetchCountries(name);
+
+  // .then(renderCountryCard);
+
+  // .catch(onFetchError);
 }
+
+// name.official,capital,population,flags.svg,languages
+
+function fetchCountries(name) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  ).then(response => {
+    response.json();
+    console.log(response.json());
+  });
+}
+
+// function renderCountryCard(name) {}
