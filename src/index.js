1deef92df6ded0f1435e037191c773da02279da4
YevgenMalashenko <malashenkoyevgen1986@gmail.com>
2022-08-25 18:43:19 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './css/styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import debounce from 'lodash.debounce';
+import { fetchCountries } from './js-modules/fetchCountries';
 
 // import countryCardTpl from '../src/country-list.hbs';
 
@@ -14,41 +15,55 @@ inputEl.addEventListener('input', debounce(onFormInput, DEBOUNCE_DELAY));
 
 function onFormInput(event) {
   event.preventDefault();
-  const name = event.target.value;
-  fetchCountries(name)
-    .then(renderCountryCard)
-    .catch(onError)
-    .finally(console.log('Ura'));
-}
-
-function fetchCountries(name) {
-  return fetch(
-    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
-  ).then(response => response.json());
+  const countryName = event.target.value;
+  fetchCountries(countryName).then(renderCountryCard).catch(onError);
 }
 
 function renderCountryCard(country) {
-  console.log(country.length);
-  let markup;
+  let markup = '';
 
   if (country.length > 10) {
+    resetMarkuUp();
     Notify.info('Too many matches found. Please enter a more specific name.');
   } else if (country.length > 1 && country.length <= 10) {
-    country.forEach((el, idx, arr) => {
-      markup += `<li class = "country__item"><img src="${el.flags.svg}" alt="flag" width="60" /><p class="country__name">${el.name.official}</p>`;
+    resetMarkuUp();
+    country.forEach(el => {
+      markup += `<li class = "country__item"><img class = "country__flag--list" src="${el.flags.svg}" alt="flag" width="60" /><p class="country__name--list">${el.name.official}</p>`;
     });
     countryListEl.innerHTML = markup;
   } else if ((country.length = 1)) {
-    // console.log(country);
+    console.log(Object.values(country[0].languages).join(', '));
+    resetMarkuUp();
+    markup = `
+    <div class = "country__title--item">
+    <img class = "country__flag--item" src="${
+      country[0].flags.svg
+    }" alt="flag" width="60" />
+      <p class="country__name--item">${country[0].name.official}</p>
+    </div>
 
-    const markup = `<img src="${country[0].flags.svg}" alt="flag" width="100" />
-      <p class="country__name">${country[0].name.official}</p>
-      <p class="country__capital">${country[0].capital[0]}</p>
-      <p class="country__population">${country[0].population}</p>`;
-    countryListEl.innerHTML = markup;
+      <p class="country__details"><span class = "country__descr"> Capital: </span>${
+        country[0].capital[0]
+      }</p>
+      <p class="country__details"><span class = "country__descr"> Population: </span>${
+        country[0].population
+      }</p>
+
+      <p class="country__details"><span class = "country__descr"> Languages: </span>${Object.values(
+        country[0].languages
+      ).join(', ')}</p>
+      `;
+    countryInfoEl.innerHTML = markup;
   }
 }
 
 function onError() {
+  resetMarkuUp();
   Notify.failure('Oops, there is no country with that name');
+}
+
+function resetMarkuUp() {
+  markup = '';
+  countryInfoEl.innerHTML = markup;
+  countryListEl.innerHTML = markup;
 }
